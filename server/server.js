68d93079818f33655d616814b2af678f460bcdd6
Brian Kiprono <89153335+bkiprono@users.noreply.github.com>
2022-09-07 18:38:58 +0300

    fix: cleaned up the server.js file

--- server/server.js
+++ server/server.js
@@ -1,16 +1,13 @@
-const { Storage } = require('@google-cloud/storage');
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const storage = new Storage();
-const myBucket = storage.bucket('my-bucket');
 
-app.get('/video', (req, res) => {
+app.get('/streaming', (req, res) => {
   const range = req.headers.range; // The current position we are in the video we are playing (This is in bytes)
-  const videoPath = 'data-store/2.mp4'; // Path to the video in our storage
+  const videoPath = 'data-store/3.mp4'; // Path to the video in our storage
   const videoSize = fs.statSync(videoPath).size; // Total Video Size
 
-  const chunkSize = 1 * 1e+6; // Amount of data to send per request
+  const chunkSize = 1 * 1e+6; // Amount of data to send per request (1MB)
   const start = range ? Number(range.replace(/\D/g, '')) : 0; // Get a number from our range by deleting chars in the range
   const end = Math.min(start + chunkSize, videoSize - 1);
   const contentLength = end - start + 1;
@@ -23,7 +20,7 @@ app.get('/video', (req, res) => {
   }
 
   res.writeHead(206, headers);
-
+  // Create a readstream from the prepared headers
   const stream = fs.createReadStream(videoPath, { start, end });
   stream.pipe(res);
 })
